9f0c3ac8646dfcd54a888807b0366cd006536cc2
Teddy LANDRON <teddy.landron@gmail.com>
2019-01-17 11:25:15 +0100

    parts of main script condensed as separate scripts/functions

---
+++ finalplots.m
@@ -0,0 +1,71 @@
+function finalplots(f_dout, f_nsubjsubset, f_nsim, f_str_nsubjsubset, f_formatSpec_file, f_param_diff_subjave, f_strparam, f_strparam_short)
+
+
+
+
+f_formatSpec_title = 'Difference recovered-fixed %s (2x%d simulations)';
+
+lgd = cell(length(f_nsubjsubset),1);
+for irevindexsubset = 1:length(f_nsubjsubset)
+   lgd{irevindexsubset}= sprintf('%d subjects',f_nsubjsubset(irevindexsubset));
+end
+
+
+
+
+
+h = figure; % y = number of stimulations
+hold on
+name_title = sprintf(f_formatSpec_title, f_strparam, f_nsim);
+title(name_title);
+
+nbins = 50;
+[n, x] = hist(f_param_diff_subjave,nbins);  
+plot(x, n, 'LineWidth',2)
+
+ylabel('Number of simulations');
+xlabel(['Difference recovered-fixed, ',f_strparam]);
+legend(lgd);
+hold off
+
+
+name_fig = sprintf(f_formatSpec_file, f_dout,['diff_',f_strparam_short,'_recov-fix'],f_nsim,f_str_nsubjsubset,'.png');
+saveas(h, name_fig) 
+
+
+
+
+clear n y
+h = figure; % with normalised distribution, AUC = 1
+hold on
+name_title = sprintf(f_formatSpec_title, f_strparam, f_nsim);
+title(name_title);
+
+nbins = 1000;
+[~, x_norm] = hist(f_param_diff_subjave,nbins);
+y = zeros(nbins, length(f_nsubjsubset));
+for irevindexsubset = 1:length(f_nsubjsubset)
+   pd = fitdist(f_param_diff_subjave(:,irevindexsubset), 'Normal');
+   y(:,irevindexsubset) = pdf(pd,x_norm);
+end
+plot(x_norm, y,'LineWidth',2)
+
+set(gca,'ColorOrderIndex',1) % to reset the default color order
+nbins = 50;
+[n, x] = hist(f_param_diff_subjave,nbins);
+empirical_pd = zeros(nbins, length(f_nsubjsubset));
+for irevindexsubset = 1:length(f_nsubjsubset)
+   empirical_pd(:,irevindexsubset) = n(:,irevindexsubset)/trapz(x,n(:,irevindexsubset)); % so that AUC = 1
+end
+plot(x, empirical_pd,':','LineWidth',1)
+
+ylabel('???');
+xlabel(['Difference recovered-fixed, ',f_strparam]);
+legend(lgd);
+hold off
+
+
+name_fig = sprintf(f_formatSpec_file, f_dout,['diff_',f_strparam_short,'_recov-fix'],f_nsim,f_str_nsubjsubset,'.png');
+saveas(h, name_fig)
+
+end
---
+++ initialfit.m
@@ -0,0 +1,35 @@
+
+function [outf_prev_fit, outf_beta_fit] = initialfit(f_nrep, f_nsubj, f_dat, f_subjlist)
+%     nrep = 1e1;
+
+    outf_prev_fit = zeros(f_nsubj,2,f_nrep);
+    outf_beta_fit = zeros(f_nsubj,2,f_nrep);
+
+    for irep = 1:f_nrep
+        for isubj = 1:f_nsubj
+            %isubj
+            for itask = 1:2
+
+                % filter trials of interest
+                ifilt = f_dat.subj == f_subjlist(isubj) & f_dat.task == itask;
+
+                % fit model
+                cfg_fit        = [];
+                cfg_fit.seqllr = f_dat.evid(ifilt);
+                cfg_fit.seqind = f_dat.sind(ifilt);
+                cfg_fit.rbef   = f_dat.rbef(ifilt);
+                cfg_fit.raft   = f_dat.raft(ifilt);
+                cfg_fit.brep   = 0; % force no repetition bias
+                cfg_fit.epsi   = 0; % force no lapses
+
+                out_fit = fit_model_softmax(cfg_fit);
+
+                % store model parameters
+                outf_prev_fit(isubj,itask,irep) = out_fit.prev;
+                outf_beta_fit(isubj,itask,irep) = out_fit.beta;
+
+            end
+        end
+    end
+end
+
